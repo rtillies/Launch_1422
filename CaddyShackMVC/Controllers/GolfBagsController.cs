342a2ec6951eb58564adbed3356cbd7ad469a70a
Richard T <richard@turing.edu>
2023-08-03 10:41:12 -0400

    User story 1

---
+++ CaddyShackMVC/Controllers/GolfBagsController.cs
@@ -0,0 +1,21 @@
+﻿using CaddyShackMVC.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using CaddyShackMVC.Models;
+
+namespace CaddyShackMVC.Controllers
+{
+    public class GolfBagsController : Controller
+    {
+        private readonly CaddyShackContext _context;
+        public GolfBagsController(CaddyShackContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var golfBags = _context.GolfBags.ToList();
+            return View(golfBags);
+        }
+    }
+}
